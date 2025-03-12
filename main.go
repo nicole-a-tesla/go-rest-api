e9aa5f2c110ecf9216881a8b9e1ddf053ebf7d24
nicole mccabe <thisisnotnicole@gmail.com>
2025-03-12 11:43:17 +0000

    GET events, GET event, and POST event + sqlite setup (course sections 160-174)

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"example.com/db"
+	"example.com/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db.InitDB()
+	server := gin.Default()
+	routes.RegisterRoutes(server)
+	server.Run(":8080")
+}
