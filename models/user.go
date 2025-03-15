962a4219038ffde7fbacbf78154ad10005788b89
nicole mccabe <thisisnotnicole@gmail.com>
2025-03-15 15:06:46 +0000

    create users

---
+++ models/user.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"example.com/db"
+	"example.com/utils"
+)
+
+type User struct {
+	ID       int64
+	Email    string `binding:"required"`
+	Password string `binding:"required"`
+}
+
+func (u User) Save() error {
+	query := "INSERT INTO users(email, password) VALUES (?, ?)"
+	statement, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer statement.Close()
+	hashedPassword, err := utils.HashPassword(u.Password)
+
+	if err != nil {
+		return err
+	}
+
+	result, err := statement.Exec(u.Email, hashedPassword)
+
+	if err != nil {
+		return err
+	}
+
+	userId, err := result.LastInsertId()
+
+	u.ID = userId
+	return err
+}
